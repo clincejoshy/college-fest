6d83e44ae6787e59b3c598aa7f5c56cc361b5bde
clincejoshy <clincepro@gmail.com>
2017-08-23 23:25:09 +0530

    edited home page including timeline

---
+++ assets/data-albe-timeline.js
@@ -0,0 +1,92 @@
+//Json Object
+var data = [
+	{
+		time: '2018-03-29',
+		body: [{
+			tag: 'h3',
+			content: '',
+			attr: {
+				cssclass: 'group-title'
+			}
+		},
+		{
+			tag: 'span',
+			content: 'Last date for full paper submission',
+			attr: {
+				cssclass: 'group-sub-title'
+			}
+		},
+		{
+			tag: 'p',
+			content: ''
+		}]
+	},
+	{
+		time: '2018-05-15',
+		body: [{
+			tag: 'h3',
+			content: '',
+			attr: {
+				cssclass: 'group-title'
+			}
+		},
+		{
+			tag: 'span',
+			content: 'Intimation of acceptance',
+			attr: {
+				cssclass: 'group-sub-title'
+			}
+		},
+		{
+			tag: 'p',
+			content: ''
+		}]
+	},
+	{
+		time: '2018-06-01',
+		body: [{
+			tag: 'h3',
+			content: '',
+			attr: {
+				cssclass: 'group-title'
+			}
+		},
+		{
+			tag: 'span',
+			content: 'Last date of camera ready paper submission',
+			attr: {
+				cssclass: 'group-sub-title'
+			}
+		},
+		{
+			tag: 'p',
+			content: ''
+		}]
+	},
+	{
+		time: '2018-05-25',
+		body: [{
+			tag: 'h3',
+			content: '',
+			attr: {
+				cssclass: 'group-title'
+			}
+		},
+		{
+			tag: 'span',
+			content: 'Last date for registration',
+			attr: {
+				cssclass: 'group-sub-title'
+			}
+		},
+		{
+			tag: 'p',
+			content: ''
+		}]
+	}];
+
+$(document).ready(function () {
+
+	$('#myTimeline').albeTimeline(data);
+
+});
